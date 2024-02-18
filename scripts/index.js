79b79ea82f9745051a51dc8db8d6013a4f301158
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-02-18 13:08:32 +0300

    for review

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,39 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
 
 // @todo: DOM узлы
 const cardPlayList = document.querySelector('.places__list');
 
-let cardTitle = cardItem.querySelector('.card__title');
-console.log(cardTitle.textContent);
-
-
-  function addCard(cardImage) {
-    cardItem.querySelector('.card__image').src = cardImage;
-    cardItem.querySelector('.card__title').textContent = cardTitle
-    cardPlayList.append(cardItem);
-   };
-
-   addCard(initialCards.forEach (function(element){
-    cardImage = element.link;
-    cardTitle = element.name;
-   }))
-
-   addButton.addEventListener('click', addSong);
-
 // @todo: Функция создания карточки
-// function addCard(cardImage) {
-  
-//       initialCards.forEach(function(element){
-//       cardImage.src = element.link;
-
-//       // cardTitle.textContent = `${element.name}`;
-//   });
-  
-//   return cardItem
- 
-// };
-// cardPlayList.append(addCard(cardImage));
-
-
- 
-
-
-
-
+initialCards.forEach(function addCard(item) {
+  let cardItem = cardTemplate.querySelector('.card').cloneNode(true); //клонировать шаблон
+  cardItem.querySelector('.card__image').src = item.link; //установить значения вложенных элементов
+  cardItem.querySelector('.card__image').alt = item.link;
+  cardItem.querySelector('.card__title').textContent = item.name;
 // @todo: Функция удаления карточки
-
+  const delButton = cardItem.querySelector('.card__delete-button');
+  delButton.addEventListener('click', delCard); //добавить обработчик клика для удаления карточки
+    function delCard(evt) {
+    const delIcon = evt.target.closest('.card');
+    delIcon.remove();
+  };
 // @todo: Вывести карточки на страницу
+  cardPlayList.append(cardItem)
+});
+  
+    
+    
+    
+  
+
+
+
+
+ 
+
+
+
+
+
+
+
