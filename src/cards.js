56cf297fd12da7c79f2318cdf55deec01ce314ff
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-03-12 22:10:04 +0300

    webpack done, first step done

--- src/cards.js
+++ src/cards.js
@@ -1,26 +1,36 @@
+const arkhyzLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url);
+const chelyabLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', import.meta.url);
+const ivanovoLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', import.meta.url);
+const kamchatkaLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', import.meta.url);
+const kholmogorLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', import.meta.url);
+const baikalLink = new URL ('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', import.meta.url);
+
 const initialCards = [
     {
       name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      link: arkhyzLink,
     },
     {
       name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      link: chelyabLink,
     },
     {
       name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      link: ivanovoLink,
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      link: kamchatkaLink,
     },
     {
       name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      link: kholmogorLink,
     },
     {
       name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      link: baikalLink,
     }
 ];
+
+
+export { initialCards };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './pages/index.css';
+import { initialCards } from './cards';
 
 
 // @todo: Темплейт карточки
@@ -6,17 +8,23 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardContainer = document.querySelector('.places__list');
 let card = cardTemplate.querySelector('.card');
+const popupNewCard = document.querySelector('.popup_type_new-card');//поле новой карточки
+const profileEditButton = document.querySelector('.profile__edit-button'); //кнопка редактирования профиля
+const newCardButton = document.querySelector('.profile__add-button'); //кнопка добавления карточки
 
 // @todo: Функция создания карточки
-function createCard(data, onDelete) {
+function createCard(data, onDelete, onPopup) {
   const cardItem = card.cloneNode(true);                                //клонировать шаблон
   const cardImage = cardItem.querySelector('.card__image');             //установить значения вложенных элементов
   const cardTitle = cardItem.querySelector('.card__title');
   const delButton = cardItem.querySelector('.card__delete-button');
-  cardImage.src = data.link
-  cardImage.alt = data.link;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
   cardTitle.textContent = data.name;
   delButton.addEventListener('click', onDelete);                    //добавить обработчик клика для любой обработки  карточки
+  cardImage.addEventListener('click', ()=>{
+    onPopup(data);
+  });
   card = cardItem
   return card;
 }
@@ -26,17 +34,62 @@ function createCard(data, onDelete) {
     const delIcon = event.target.closest('.card');
     delIcon.remove();
   };
-      
+
+   //функция выведения поля добавления
+   function showCardContent(evt) {
+    const cardContent = document.querySelector('.popup_type_image');
+    const cardImage = cardContent.querySelector('.popup__image');
+    const cardText = cardContent.querySelector('.popup__caption');
+    const popupCloseButton = cardContent.querySelector('.popup__close');
+    
+    cardContent.classList.add('popup_is-opened');
+    cardImage.src = evt.link;
+    cardImage.alt = evt.name;
+    cardText.textContent = evt.name;
+
+    popupCloseButton.addEventListener('click', function() {
+      onClose(popupCloseButton);
+    });
+  }
+
 // @todo: Вывести карточки на страницу    
 initialCards.forEach(function (item) {                              
- createCard(item, deleteCard);                                             //присвоить функции переменную из массива initialCards и функцию удаления карточки в качестве колбэка
+ createCard(item, deleteCard, showCardContent);                                             //присвоить функции переменную из массива initialCards и функцию удаления карточки в качестве колбэка
  cardContainer.append(card);
 });
 
+//открытие поля "редактировать профиль" по кнопке "редактировать" 
+profileEditButton.addEventListener('click', showPopupEdit);
 
-   
+  //функция выведения поля редактирования
+function showPopupEdit() {
+   const popupEdit = document.querySelector('.popup_type_edit')
+   popupEdit.classList.add('popup_is-opened');
 
- 
+   const popupCloseButton = popupEdit.querySelector('.popup__close');
+   popupCloseButton.addEventListener('click', function() {
+    onClose(popupCloseButton);
+   });
+}
+
+//открытие поля "новая карточка" по кнопке "добавить" 
+newCardButton.addEventListener('click', showPopupAdd);
+
+  //функция выведения поля добавления
+  function showPopupAdd() {
+    const popupAdd = document.querySelector('.popup_type_new-card')
+    popupAdd.classList.add('popup_is-opened');
+
+    const popupCloseButton = popupAdd.querySelector('.popup__close');
+    popupCloseButton.addEventListener('click', function() {
+      onClose(popupCloseButton);
+    });
+  }
+
+  function onClose(evt) {
+    const popupToClose = evt.closest('.popup'); 
+    popupToClose.classList.remove('popup_is-opened');
+  }
 
 
 
