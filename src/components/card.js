40f718a555bf38e133ca57d3bf14fb4416768eb9
Jenia Shalomeenko <evgeniyashalomeenko@yandex.ru>
2024-03-18 15:15:53 +0300

    to review first attempt

--- src/components/card.js
+++ src/components/card.js
@@ -1,33 +1,13 @@
-import { handleModalCard } from "./modals";
+import { closeModalCard } from "./modals";
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 const cardElement = cardTemplate.querySelector('.card');
 
-// Функция закрытия окна  
-export function onClose(evt) {
-  const popupToClose = evt.closest('.popup'); 
-  popupToClose.classList.remove('popup_is-opened');
-}
-
 // Функция лайка
 export function handleLike(icon) {
   icon.classList.toggle('card__like-button_is-active');
 }
-
-// Функция выведения большой карточки
-export function showCardContent(evt) {
-  const cardContent = document.querySelector('.popup_type_image');
-  const cardLargeImage = cardContent.querySelector('.popup__image');
-  const cardCaption = cardContent.querySelector('.popup__caption');
-  const popupCloseButton = cardContent.querySelector('.popup__close');
-    
-  cardLargeImage.src = evt.link;
-  cardLargeImage.alt = evt.name;
-  cardCaption.textContent = evt.name;
-
-  handleModalCard(cardContent, popupCloseButton);
-};
 
 // @todo: Функция удаления карточки
 export function deleteCard(event) {
@@ -37,15 +17,15 @@ export function deleteCard(event) {
 
 // @todo: Функция создания карточки
 export function createCard(data, onDelete, onPopup, onLike) {
-  const cardItem = cardElement.cloneNode(true);                                //клонировать шаблон
-  const cardImage = cardItem.querySelector('.card__image');             //установить значения вложенных элементов
+  const cardItem = cardElement.cloneNode(true);                                
+  const cardImage = cardItem.querySelector('.card__image');             
   const cardTitle = cardItem.querySelector('.card__title');
   const delButton = cardItem.querySelector('.card__delete-button');
   const likeButton = cardItem.querySelector('.card__like-button');
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
-  delButton.addEventListener('click', onDelete);                    //добавить обработчик клика для любой обработки  карточки
+  delButton.addEventListener('click', onDelete);                    
   cardImage.addEventListener('click', ()=>{
     onPopup(data);
   });
@@ -54,3 +34,4 @@ export function createCard(data, onDelete, onPopup, onLike) {
   });
   return cardItem;
 }
+
--- src/components/modals.js
+++ src/components/modals.js
@@ -1,47 +1,59 @@
-import { onClose } from "./card";
-// Функция сабмита модального окна "добавить новую карточку"
-export function addPlaceSubmit(place, url, onCreate, onDelete, onPopup, container, element, nameField, closeCard){
-  const item = {};
-  item.name = place.value;
-  item.link = url.value;
-  onCreate(item, onDelete, onPopup);                                           
-  container.prepend(element);
-  const formToClose = nameField.closest('.popup');
-  closeCard (formToClose);
-}
+const cardContent = document.querySelector('.popup_type_image');
+const cardLargeImage = cardContent.querySelector('.popup__image');
+const cardCaption = cardContent.querySelector('.popup__caption');
 
-// Функция сабмита модального окна "редактировать профиль"
-export function handleFormSubmit(title, nameField, description, jobField, closeCard){
-  title.textContent = nameField.value;
-  description.textContent = jobField.value;
-  const formToClose = nameField.closest('.popup');
-  closeCard (formToClose);
-}
-
-export function openModal(form, title, description) {
-  const elmName = form.elements.name;
-  const elmDescription = form.elements.description; 
-  elmName.value = title.textContent;
-  elmDescription.value = description.textContent;
-}
-
-// Функция открытия и закрытия модального окна 
-export function handleModalCard (modalCard, button) {
+// Функция открытия модального окна
+export function openModalCard (modalCard) {
   modalCard.classList.add('popup_is-opened');
+} 
 
-  button.addEventListener('click', function() {
+// Функция закрытия окна  
+export function onClose(evt) {
+  const popupToClose = evt.closest('.popup'); 
+  popupToClose.classList.remove('popup_is-opened');
+}
+
+// Функция закрытия модального окна всеми тремя способами
+export function closeModalCard (modalCard) {
+  const button = modalCard.querySelector('.popup__close');
+
+  button.addEventListener('click', function(){
     onClose(button);
   });
-
   window.addEventListener('keydown', function(e) {
     if(e.key === 'Escape') {
       onClose(modalCard);
       };
     });
-
   modalCard.addEventListener('click', function(evt){
     if (evt.target.classList.contains('popup')) {
       onClose(modalCard)
     };
   });
 }
+
+// Функция сабмита модального окна "редактировать профиль"
+export function handleEditFormSubmit(title, nameField, description, jobField, closeCard){
+  title.textContent = nameField.value;
+  description.textContent = jobField.value;
+  const formToClose = nameField.closest('.popup');
+  closeCard (formToClose);
+}
+
+// Функция сабмита модального окна "добавить новую карточку"
+export function addPlaceSubmit(place, url){
+  const item = {};
+  item.name = place.value;
+  item.link = url.value;
+  return item;
+}
+
+// Функция выведения большой карточки
+export function showCardContent(item) {
+  cardLargeImage.src = item.link;
+  cardLargeImage.alt = item.name;
+  cardCaption.textContent = item.name;
+  openModalCard(cardContent);
+  closeModalCard(cardContent);
+};
+
